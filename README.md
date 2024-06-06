b853a77c6a660481190d4ff0619030da6bc3f464
Iyarr <8.24ridiculous@gmail.com>
2024-06-06 14:19:55 +0900

    test code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AWS_Practice
+
+## やりたいこと
+
+AWS内のサービスを使ったAPI実装の練習
+
+## 実装するサービス
+
+- JWT Authorizer
+- API Gateway
+- Lamda
---
+++ main.tf
@@ -0,0 +1,52 @@
+# ローカルのZIPファイルを作成
+resource "null_resource" "create_lambda_zip" {
+  provisioner "local-exec" {
+    command = "zip lambda_function.zip lamda.js"
+    working_dir = "."
+  }
+
+  depends_on = [
+    local_file.lambda_js
+  ]
+}
+
+resource "local_file" "lambda_js" {
+  content  = file("./lamda.js")
+  filename = "./lamda.js"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# Lambda関数をデプロイ
+resource "aws_lambda_function" "hello_lamda" {
+  function_name    = "HelloLambdaFunction"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "main.handler"
+  runtime          = "nodejs20.x"
+  filename         = "./lambda_function.zip"
+
+  source_code_hash = filebase64sha256("./lambda_function.zip")
+
+  # ZIPファイルが変更された場合にデプロイメントをトリガー
+  depends_on = [null_resource.create_lambda_zip]
+}
+
+output "lambda_function_arn" {
+  value = aws_lambda_function.hello_lamda.arn
+}
