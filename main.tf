6897e6f9d54dd651965d84fc04956f301b60beef
Iyarr <8.24ridiculous@gmail.com>
2024-06-10 13:30:27 +0900

    implyment api  gateway

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_iam_role" "iam_for_api_gateway" {
 resource "aws_lambda_function" "hello_lambda" {
   function_name    = "HelloLambdaFunction"
   role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "main.handler"
+  handler          = "lambda.handler"
   runtime          = "nodejs20.x"
   filename         = "lambda.zip"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
