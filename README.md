6410cfb353b528eab7a41650192b296fb2c803ae
Iyarr <8.24ridiculous@gmail.com>
2024-06-10 13:29:56 +0900

    implyment api  gateway

--- README.md
+++ README.md
@@ -4,13 +4,19 @@
 
 AWS 内のサービスを使った API 実装の練習
 
-## 実装するサービス
+## 使用するサービス
 
 - JWT Authorizer
 - API Gateway
-- Lamda
+- Lambda
+- Firebase Authentication
 
 ## 使う技術
 
 - Nodejs
 - TypeScript
+
+## 作りたい API
+
+GET メソッドリクエストを送り、レスポンスとして `Hello, World!` を返す API
+レスポンスを返すかどうかを Firebase Authentication で制御する
--- main.tf
+++ main.tf
@@ -17,9 +17,27 @@ data "aws_iam_policy_document" "lambda_assume_role" {
   }
 }
 
+data "aws_iam_policy_document" "api_gateway_assume_role" {
+  statement {
+    effect  = "Allow"
+  
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+  
+    actions = ["sts:AssumeRole"]
+  }
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role" "iam_for_api_gateway" {
+  name               = "iam_for_api_gateway"
+  assume_role_policy = data.aws_iam_policy_document.iam_for_api_gateway.json
 }
 
 # Lambda関数をデプロイ
@@ -30,6 +48,34 @@ resource "aws_lambda_function" "hello_lambda" {
   runtime          = "nodejs20.x"
   filename         = "lambda.zip"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+resource "aws_api_gateway_rest_api" "practice-api" {
+  name        = "practice-api"
+
+  body = jsonencode({
+    openapi = "3.0.1"
+    info = {
+      title   = "example"
+      version = "1.0"
+    }
+    paths = {
+      "/path1" = {
+        get = {
+          x-amazon-apigateway-integration = {
+            httpMethod           = "GET"
+            payloadFormatVersion = "1.0"
+            type                 = "AWS_PROXY"
+            uri                  = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.hello_lambda.arn}/invocations"
+          }
+        }
+      }
+    }
+  })
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 output "lambda_function_arn" {
