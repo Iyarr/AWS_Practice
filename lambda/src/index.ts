a3338ee05d14fb72a4f6ba2e45663dcbaf939293
Iyarr <8.24ridiculous@gmail.com>
2024-06-12 15:21:54 +0900

    changed response string on lambda to trigger github actions

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -3,7 +3,7 @@ import { Handler } from "aws-lambda";
 export const handler: Handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda by Typescript!!"),
+    body: JSON.stringify("Hello from Lambda by Typescript!"),
   };
   return response;
 };
