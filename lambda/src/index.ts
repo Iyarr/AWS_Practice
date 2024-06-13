a8d22f4408a7cf6fd4e662d573e250f462b93195
Iyarr <8.24ridiculous@gmail.com>
2024-06-13 14:01:58 +0900

    triggered github actions

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -3,7 +3,7 @@ import { Handler } from "aws-lambda";
 export const handler: Handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!"),
+    body: JSON.stringify("Hello from Lambda!!"),
   };
   return response;
 };
