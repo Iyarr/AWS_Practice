0b59120c8c476be4867ca18063caef6a0a307ba3
Iyarr <8.24ridiculous@gmail.com>
2024-06-13 15:57:03 +0900

    backed cache path in github actions

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -7,7 +7,7 @@ import {
 export const handler: Handler = async (event: APIGatewayProxyEvent) => {
   const response: APIGatewayProxyResult = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!!"),
+    body: JSON.stringify("Hello from Lambda!"),
   };
   return response;
 };
