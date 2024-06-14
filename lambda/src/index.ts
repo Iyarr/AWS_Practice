9dfee4da8a749559c6218b17ead0d2e0523f1dd5
Iyarr <8.24ridiculous@gmail.com>
2024-06-14 12:14:30 +0900

    deleted git flow for push repository

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -1,5 +1,7 @@
-export const handler: any = async (event: any) => {
-  const response: any = {
+import { APIGatewayProxyResult, Handler, APIGatewayEvent } from "aws-lambda";
+
+export const handler: Handler = async (event: APIGatewayEvent) => {
+  const response: APIGatewayProxyResult = {
     statusCode: 200,
     body: JSON.stringify("Hello from Lambda!"),
   };
