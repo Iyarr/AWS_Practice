c43be38cbdc8bfc412adda55366a4f0a93f46903
Iyarr <8.24ridiculous@gmail.com>
2024-06-17 22:29:31 +0900

    #46 modified a varible typo

--- api_gateway/main.tf
+++ api_gateway/main.tf
@@ -26,7 +26,7 @@ resource "aws_api_gateway_integration" "default" {
   http_method             = aws_api_gateway_method.default.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = var.hello_lambda_invoke_arn.value
+  uri                     = var.hello_lambda_invoke_arn
   credentials = aws_iam_role.default.arn
 }
 
