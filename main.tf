86afb7713e5de997161372f708a3fc3549191f3c
Iyarr <8.24ridiculous@gmail.com>
2024-06-20 14:30:51 +0900

    attached default tags to all resources

--- main.tf
+++ main.tf
@@ -14,6 +14,12 @@ provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
   region     = var.region
+
+  default_tags {
+    tags = {
+      name = "This is a test tag"
+    }
+  }
 }
 
 output "api_gateway_invoke_url" {
