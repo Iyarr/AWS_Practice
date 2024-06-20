4ff6979810ec897caeb031e87c93a03da341bb6d
Iyarr <8.24ridiculous@gmail.com>
2024-06-20 16:22:55 +0900

    modified resource names in api_gateway

--- api_gateway/policies.tf
+++ api_gateway/policies.tf
@@ -1,34 +1,34 @@
 # Don't duplicate reource names to other modules.
-resource "aws_iam_role" "api_gateway" {
-  name               = var.prefix + "iam_for_api_gateway"
+resource "aws_iam_role" "integration" {
+  name               = var.prefix + "role_for_api_gateway_integration"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
-resource "aws_iam_role" "api_gateway_logging" {
-  name               = var.prefix + "iam_for_logging"
+resource "aws_iam_role" "logs" {
+  name               = var.prefix + "iam_for_logs"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 # Attached to the API Gateway role
-resource "aws_iam_policy" "api_gateway" {
-  name        = var.prefix + "api_gateway"
+resource "aws_iam_policy" "integration" {
+  name        = var.prefix + "api_gateway_integration"
   description = "Allow API Gateway to invoke Lambda"
   policy = data.aws_iam_policy_document.default.json
 }
 
 resource "aws_iam_role_policy_attachment" "default" {
-  role       = aws_iam_role.api_gateway.name
-  policy_arn = aws_iam_policy.api_gateway.arn
+  role       = aws_iam_role.integration.name
+  policy_arn = aws_iam_policy.integration.arn
 }
 
 # Logging policy
-resource "aws_iam_policy" "api_gateway_logging" {
-  name        = var.prefix + "api_gateway_logging"
+resource "aws_iam_policy" "logs" {
+  name        = var.prefix + "api_gateway_logs"
   description = "IAM policy for logging from a api_gateway"
   policy      = data.aws_iam_policy_document.api_gateway_logging.json
 }
 
-resource "aws_iam_role_policy_attachment" "api_gateway_logs" {
-  role       = aws_iam_role.api_gateway_logging.name
-  policy_arn = aws_iam_policy.api_gateway_logging.arn
+resource "aws_iam_role_policy_attachment" "logs" {
+  role       = aws_iam_role.logs.name
+  policy_arn = aws_iam_policy.integration.arn
 }
