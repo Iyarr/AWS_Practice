8235164f56ab2c9329de1d353942837bd0a2f550
Iyarr <8.24ridiculous@gmail.com>
2024-07-02 21:59:18 +0900

    set buildspec in codebuild

--- lambda/codebuild.tf
+++ lambda/codebuild.tf
@@ -21,6 +21,7 @@ resource "aws_codebuild_project" "npm_build" {
 
   source {
     type = "CODEPIPELINE"
+    buildspec = file("${path.module}/buildspec.yml")
   }
 
   service_role = aws_iam_role.codebuild_service_role.arn
