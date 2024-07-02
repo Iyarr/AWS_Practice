2666ed398715652e53fc684070faf5606024f9d8
Iyarr <8.24ridiculous@gmail.com>
2024-07-02 10:44:30 +0900

    created events full access

--- lambda/codepipeline.tf
+++ lambda/codepipeline.tf
@@ -168,7 +168,22 @@ resource "aws_iam_role" "events_role" {
   })
 }
 
+resource "aws_iam_policy" "events_full_access" {
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "events:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "events_policy_attachment" {
   role       = aws_iam_role.events_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEventBridgeFullAccess"
+  policy_arn = aws_iam_policy.events_full_access.arn
 }
