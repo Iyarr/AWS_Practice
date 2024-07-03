535f43ac275f43126bbd8c3029b0d4e49af36303
Iyarr <8.24ridiculous@gmail.com>
2024-07-03 10:41:03 +0900

    made lambda get source code from s3 on init

--- lambda/main.tf
+++ lambda/main.tf
@@ -3,7 +3,8 @@ resource "aws_lambda_function" "hello_lambda" {
   role             = aws_iam_role.default.arn
   handler          = "index.handler"
   runtime          = "nodejs20.x"
-  filename         = data.archive_file.init_zip.output_path
+  s3_bucket = aws_s3_bucket.app.bucket
+  s3_key = aws_s3_object.source.key
 
   logging_config {
     log_group = aws_cloudwatch_log_group.default.name
@@ -11,7 +12,7 @@ resource "aws_lambda_function" "hello_lambda" {
     system_log_level = "INFO"
   }
 
-  source_code_hash = data.archive_file.init_zip.output_base64sha256
+  source_code_hash = aws_s3_object.source.key
 
   depends_on = [
     aws_iam_role_policy_attachment.logs
