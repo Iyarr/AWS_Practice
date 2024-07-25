4215d9e0872cfc3e72d0097781b546f5966e91b6
Iyarr <8.24ridiculous@gmail.com>
2024-07-25 18:20:53 +0900

    #106 アクセスキーの出力設定

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 output "unvoke_url" {
   value = aws_api_gateway_deployment.default.invoke_url
 }
+
+output "ci_access_key_id" {
+  value = module.ci.aws_access_key_id
+  sensitive = true
+}
+
+output "ci_secret_access_key" {
+  value = module.ci.aws_secret_access_key
+  sensitive = true
+}
